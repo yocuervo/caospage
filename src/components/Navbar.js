c674aa0b580a88a0eb8d06c17801452ece3f3322
Exequiel <yo.cuervo.yo@gmail.com>
2022-08-30 14:06:12 -0300

    probando rama test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
         <div className='navbar__container'>
             <div className='button__container'>
                 <span></span>
-                <button type="button" className='btn btn-outline-dark navbar__buttons'>Orígenes</button>
+                {/* <button type="button" className='btn btn-outline-dark navbar__buttons'>Orígenes</button> */}
                 <button type="button" className='btn btn-outline-dark navbar__buttons'>Integrantes</button>
                 <button type="button" className='btn btn-outline-dark navbar__buttons'>Juegos</button>
                 <button type="button" className='btn btn-outline-dark navbar__buttons'>Redes</button>
